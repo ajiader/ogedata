27e1361b91bbb8a4b43748bc5b7337765f541e27
wangxingchao <sosowangxingchao@126.com>
2015-04-18 21:38:30 +0800

    频道页样式修改

--- api/pages.default.php
+++ api/pages.default.php
@@ -3,7 +3,7 @@ defined('IN_DESTOON') or exit('Access Denied');
 $_page = $page <= 1 ? $total : ($page - 1);
 $demo_url = str_replace(array('%7B', '%7D'), array('{', '}'), $demo_url);
 $url = $_page == 1 ? $home_url : str_replace('{destoon_page}', $_page, $demo_url);
-$pages .= '<input type="hidden" id="des'.'toon_previous" value="'.$url.'"/><a href="'.$url.'">&nbsp;&#171;'.$L['prev_page'].'&nbsp;</a> ';
+$pages .= '<input type="hidden" id="des'.'toon_previous" value="'.$url.'"/><a href="'.$url.'" class="pageleft">&nbsp;&#171;'.$L['prev_page'].'&nbsp;</a> ';
 if($total >= 1) {
 	$_page = 1;
 	$url = $home_url;
@@ -15,7 +15,7 @@ if($total >= 2) {
 	$pages .= $page == $_page ? '<strong>&nbsp;'.$_page.'&nbsp;</strong> ' : ' <a href="'.$url.'">&nbsp;'.$_page.'&nbsp;</a>  ';
 }
 if($total >= 3) {
-	$pages .= '&nbsp;&#8230;&nbsp;';
+	$pages .= '<b>&nbsp;&#8230;&nbsp;</b>';
 	if($total > 4) {
 		if($page <= 2) {
 			$min = 3; $max = 3 + $step*2;
@@ -34,7 +34,7 @@ if($total >= 3) {
 			$url = $_page == 1 ? $home_url : str_replace('{destoon_page}', $_page, $demo_url);
 			$pages .= $page == $_page ? '<strong>&nbsp;'.$_page.'&nbsp;</strong> ' : ' <a href="'.$url.'">&nbsp;'.$_page.'&nbsp;</a>  ';
 		}
-		$pages .= '&nbsp;&#8230;&nbsp;';
+		$pages .= '<b>&nbsp;&#8230;&nbsp;</b>';
 	}
 	if($total >= 4) {
 		$_page = $total - 1;
@@ -49,6 +49,6 @@ if($total >= 3) {
 }
 $_page = $page >= $total ? 1 : $page + 1;
 $url = $_page == 1 ? $home_url : str_replace('{destoon_page}', $_page, $demo_url);
-$pages .= '<a href="'.$url.'">&nbsp;'.$L['next_page'].'&#187;&nbsp;</a> <input type="hidden" id="des'.'toon_next" value="'.$url.'"/>&nbsp;'.lang($L['info_page'], array($items, $total)).'&nbsp;';
+$pages .= '<a href="'.$url.'" class="pageright">&nbsp;'.$L['next_page'].'&#187;&nbsp;</a> <input type="hidden" id="des'.'toon_next" value="'.$url.'"/>&nbsp;'.lang($L['info_page'], array($items, $total)).'&nbsp;';
 $pages .= '<input type="text" class="pages_inp" id="destoon_pageno" value="'.$page.'" onkeydown="if(event.keyCode==13 && this.value) {window.location.href=\''.$demo_url.'\'.replace(/\\{destoon_page\\}/, this.value);return false;}"> <input type="button" class="pages_btn" value="GO" onclick="if(Dd(\'destoon_pageno\').value>0)window.location.href=\''.$demo_url.'\'.replace(/\\{destoon_page\\}/, Dd(\'destoon_pageno\').value);"/>';
 ?>
