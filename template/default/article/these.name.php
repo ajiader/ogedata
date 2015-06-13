4ec419946cd0c5290c9588441500b4acb9bdca44
ajiader <310255981@qq.com>
2015-06-13 16:57:24 +0800

    修改模板有几个新的

--- template/default/article/these.name.php
+++ template/default/article/these.name.php
@@ -3,6 +3,7 @@ $names = array (
   'index' => '首页',
   'index-zy' => 'index_zy',
   'list' => '列表页',
+  'list-product' => '产品资讯列表',
   'list-zy' => 'list',
   'search' => '搜索页',
   'search-zy' => 'search',
