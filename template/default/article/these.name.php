ed81874afe4ff653053fe0dc8e16222a36e1d7df
guojia <310255981@qq.com>
2015-06-19 17:46:04 +0800

    模板修改

--- template/default/article/these.name.php
+++ template/default/article/these.name.php
@@ -4,9 +4,11 @@ $names = array (
   'index-zy' => 'index_zy',
   'list' => '列表页',
   'list-product' => '产品资讯列表',
+  'list-zhaobiao' => 'list招标',
   'list-zy' => 'list',
   'search' => '搜索页',
   'search-zy' => 'search',
+  'show-zhaobiao' => 'show招标',
   'show-zy' => '内容页',
 );
 ?>
